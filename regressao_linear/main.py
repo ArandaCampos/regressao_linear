dbfcf3a2b4b39125ad68a36fe9d48ffb5b1c86c3
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-07-31 12:55:40 -0300

    Versão 1.0 finish

--- regressao_linear/main.py
+++ regressao_linear/main.py
@@ -5,11 +5,12 @@ from plot_graphic import show_graphic
 file = "dados.csv"
 
 if __name__ == '__main__':
+    print('Bem-vindo(a) ao Regressão Linear')
     data = read_csv(file)
     slope, origin, correlation_coefficient = calc(data)
     print(slope, origin, correlation_coefficient)
     if correlation_coefficient:
         show_graphic(data, slope, origin)
     else:
-        print('coeficiente de correlação igual a 0 ')
+        print('Não há relação entre os dados. Coeficiente de correlação igual a 0')
 
