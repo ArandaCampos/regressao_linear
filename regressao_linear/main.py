07098c5a59108101b3e523692b31ce519018f111
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-07-31 14:46:50 -0300

    Versão 1.0 finished

--- regressao_linear/main.py
+++ regressao_linear/main.py
@@ -1,15 +1,17 @@
 from read_file import read_csv
 from linear_regression import calc
 from plot_graphic import show_graphic
+from pathlib import Path
 
 file = "dados.csv"
+print('Bem-vindo(a) ao Regressão Linear')
 
 if __name__ == '__main__':
-    print('Bem-vindo(a) ao Regressão Linear')
     data = read_csv(file)
     slope, origin, correlation_coefficient = calc(data)
-    print(slope, origin, correlation_coefficient)
+
     if correlation_coefficient:
+        print(f'Coeficiente de correlação: {correlation_coefficient:.2f}')
         show_graphic(data, slope, origin)
     else:
         print('Não há relação entre os dados. Coeficiente de correlação igual a 0')
